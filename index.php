2d1b7d021352ad8988724d9563f242c19d4bed2d
char <32-kot@mail.ru>
2020-06-02 15:53:21 +0500

    add main page generator

--- index.php
+++ index.php
@@ -17,26 +17,55 @@
 	</div>
 	<div id="content_wrapper">
 		<?php
+		$options = array(
+			PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC
+			);
+		try
+		{
+			$connection = new PDO('mysql:host=localhost;dbname=films_index;charset=utf8', 'root', 'root', $options);
+		} 
+		catch (Exception $e) 
+		{
+			exit('<script type="text/javascript">alert("Подключение к базе данных не удалось, попробуйте перезагрузить страницу: ' . $e->getMessage().'");
+			location.href=location.href;</script>');
+		}
+		$stmt = $connection->query('SELECT film.id as id, title, category.name as category, country.name as country, `release`, price  FROM category_list
+												LEFT JOIN category ON category.id = category_id 
+												LEFT JOIN film ON film.id = film_id
+												LEFT JOIN country ON film.country_id = country.id
+												ORDER BY category
+												');
+		if (!$stmt)
+			exit('<script type="text/javascript">alert("Не удалось загрузить информацию о данном фильме, попробуйте перезагрузить страницу: ");
+			location.href=location.href;</script>');
+		$data = $stmt->fetchAll();
 		$category_tmp = "";
 		$line_tmp = 0;
 
 		for ($i=0; $i < count($data); $i++) { 
 			if ($category_tmp != $data[$i]["category"])
 			{
-				$line_tmp++;
 				$category_tmp = $data[$i]["category"];
 				echo "<div class='content' id='line_".$line_tmp."'>
 				<h1>".$category_tmp."</h1>";
 			}
 			echo '<div class="preview" style="background-image: url(srcs/images/'.$data[$i]['id'].'/preview)" onclick="location.href = `/page?id='.$data[$i]['id'].'`">
-					<div class="poster_info"><h2>'.$data[$i]['title'].'</h2>
-						<div class="poster_info_params">'.$data[$i]['country'].' '.$data[$i]['year'].'<span>'.$data[$i]['price'].'р</span></div>
+					<div class="poster_info"><h2';
+			$strlen_tmp = mb_strlen($data[$i]['title']);
+			if ($strlen_tmp > 25 && $strlen_tmp < 35)
+				echo ' class="marquee_1"';
+			else if ($strlen_tmp >= 35)
+				echo ' class="marquee_2"';
+			echo '>'.$data[$i]['title'].'</h2><div class="poster_info_params">'.$data[$i]['country'].' '.$data[$i]['release'].'<span>'.$data[$i]['price'].'р</span></div>
 					</div>
 				</div>';
 			if ($category_tmp != $data[$i + 1]["category"])
 			{
-				echo '<button onclick="scrollSide('.$line_tmp - 1.', 0)" style="left: -1%"><</button> <button onclick="scrollSide('.$line_tmp - 1.', 	true)" style="right: -2%">></button>
+				echo '<button onclick="scrollSide('.$line_tmp.', 0)" style="left: -1%"><</button> <button onclick="scrollSide('.$line_tmp.', true)" style="right: -2%">></button>
 					</div>';
+				$line_tmp++;
+
 			}
 		}
 		?>
@@ -62,7 +91,7 @@
 			<button onclick="scrollSide(0, 0)" style="left: -1%"><</button> <button onclick="scrollSide(0, true)" style="right: -2%">></button>
 		</div>
  -->
-		<div class="content" id="line_2"><a href="#"></a>
+		<!-- <div class="content" id="line_2"><a href="#"></a>
 			<h1>Сериалы</h1>
 		</div>
 		<div class="content" id="line_3"><a href="#"></a>
@@ -70,7 +99,7 @@
 		</div>
 		<div class="content" id="line_4"><a href="#"></a>
 			<h1>Все</h1>
-		</div>
+		</div> -->
 	</div>
 	<div id="search_wrapper">
 		<?php include "srcs/modules/search.html"; ?>
