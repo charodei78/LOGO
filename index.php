caf048ab2dcd5dbcf198a4bdac20c2d3c1fde71a
Charodei78 <32-kot@mail.ru>
2020-06-02 12:51:57 +0500

    add part of main page generator

--- index.php
+++ index.php
@@ -16,46 +16,52 @@
 		</div>
 	</div>
 	<div id="content_wrapper">
-		<div class="content" id="line_1">
-			<h1>Фильмы</h1>
+		<?php
+		$category_tmp = "";
+		$line_tmp = 0;
 
-			<!-- <img class="preview"  onclick="location.href = 'page?id=1'" src="srcs/images/1/preview"> -->
+		for ($i=0; $i < count($data); $i++) { 
+			if ($category_tmp != $data[$i]["category"])
+			{
+				$line_tmp++;
+				$category_tmp = $data[$i]["category"];
+				echo "<div class='content' id='line_".$line_tmp."'>
+				<h1>".$category_tmp."</h1>";
+			}
+			echo '<div class="preview" style="background-image: url(srcs/images/'.$data[$i]['id'].'/preview)" onclick="location.href = `/page?id='.$data[$i]['id'].'`">
+					<div class="poster_info"><h2>'.$data[$i]['title'].'</h2>
+						<div class="poster_info_params">'.$data[$i]['country'].' '.$data[$i]['year'].'<span>'.$data[$i]['price'].'р</span></div>
+					</div>
+				</div>';
+			if ($category_tmp != $data[$i + 1]["category"])
+			{
+				echo '<button onclick="scrollSide('.$line_tmp - 1.', 0)" style="left: -1%"><</button> <button onclick="scrollSide('.$line_tmp - 1.', 	true)" style="right: -2%">></button>
+					</div>';
+			}
+		}
+		?>
+		<!-- <h1>Фильмы</h1>
+
+		<div class="preview" style="background-image: url(srcs/images/2/preview)" onclick="location.href = 'page.html'">
+			<div class="poster_info"><h2>Аватар</h2>
+				<div class="poster_info_params">США 2016<span>1000р</span></div>
+			</div>
+		</div>
+		<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'">
+			<div class="poster_info"><h2>Аватар</h2>
+				<div class="poster_info_params">США 2016<span>1000р</span></div>
+			</div>
+		</div> -->
+<!-- 
+
 			<div class="preview" style="background-image: url(srcs/images/2/preview)" onclick="location.href = 'page.html'">
 				<div class="poster_info"><h2>Аватар</h2>
 					<div class="poster_info_params">США 2016<span>1000р</span></div>
 				</div>
 			</div>
-			<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'">
-				<div class="poster_info"><h2>Аватар</h2>
-					<div class="poster_info_params">США 2016<span>1000р</span></div>
-				</div>
-			</div>
-			<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'"></div>
-			<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'"></div>
-			<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'"></div>
-			<div class="preview" style="background-image: url(srcs/images/1/preview)" onclick="location.href = 'page.html'"></div>
-<!-- 
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img  onclick="location.href = 'page.html'" src="srcs/images/1/preview">
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a>
-			<img src="srcs/images/1/preview"></a> -->
-			
-			
-			
 			<button onclick="scrollSide(0, 0)" style="left: -1%"><</button> <button onclick="scrollSide(0, true)" style="right: -2%">></button>
-			
 		</div>
+ -->
 		<div class="content" id="line_2"><a href="#"></a>
 			<h1>Сериалы</h1>
 		</div>
